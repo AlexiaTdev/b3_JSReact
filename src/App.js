90f4e534309e03740804b82ee3581b49f816a27a
AlexiaTdev <alexia.teissier1@epsi.fr>
2021-01-08 12:59:59 +0100

    save project react app js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
-
+import react, {useState} from 'react';
 import './App.css';
+import MovieHeader from './components/MovieHeader'
 
 const App = ({title}) => {
+  const [color, setColor] = useState("red")
   
+  const main_movie = {
+    title : "Avengers",
+    type : "film"
+  }
+
+  const toggleColor = () => {
+    if (color ==='red'){
+      setColor("blue")
+    } else {
+      setColor("red")
+    }
+  }
+
   return (
     <div className="App">
       <h1>{title}</h1>
+      <MovieHeader movie={main_movie} color={color}/>
+      <button onClick={toggleColor}>Changer la couleur</button>
     </div>
   );
 }
---
+++ src/components/MovieHeader.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const MovieHeader = ({movie, color}) => {
+    return(
+        <p>MOVIE HEADER {movie.title} {color}</p>
+    )
+}
+export default MovieHeader
